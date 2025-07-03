42c46b5b266d4f472cc147c6769730ac5bdc0c29
syeelakoloay <koloaysyeela@gmail.com>
2025-07-04 03:55:20 +0800

    fetch db on footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,26 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const Footer = () => {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [gitHub, setGitHub] = useState("");
+
+  useEffect(() => {
+    const db = getDatabase();
+
+    onValue(ref(db, "footer/"), (snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        setEmail(data.email);
+        setPhone(data.phone);
+        setGitHub(data.gitHub);
+      } else {
+        console.log("No data available");
+      }
+    });
+  }, []);
+
   return (
     <section id="footer" className="footer_area gray-bg pt-115 pb-120">
       <div className="container">
@@ -7,15 +29,13 @@ const Footer = () => {
             <div className="footer_content text-center">
               <ul className="footer_menu">
                 <li>
-                  <a href="mailto:koloaysyeela@gmail.com">
-                    koloaysyeela@gmail.com
-                  </a>
+                  <a href={`mailto:${email}`}>{email}</a>
                 </li>
                 <li>
-                  <a href="tel:082396700168">082396700168</a>
+                  <a href={`tel:${phone}`}>{phone}</a>
                 </li>
                 <li>
-                  <a href="https://github.com/syeelakoloay">syeelakoloay</a>
+                  <a href={`https://github.com/${gitHub}`}>{gitHub}</a>
                 </li>
               </ul>
               <p className="credit pt-45">Copyright &copy; 2025</p>
